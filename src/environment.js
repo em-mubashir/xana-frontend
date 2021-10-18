91525e9a88fc34e3db10aa24dd0c762577daaf16
malikmubashir025 <malikmubashir025@gmail.com>
2021-10-18 10:56:34 +0500

    if this commit gets mereged it will add environment file

---
+++ src/environment.js
@@ -0,0 +1,15 @@
+let BASE_URL = "http://192.168.18.14/api/";
+
+if (process.env.REACT_APP_ENV === "production") {
+  BASE_URL = "http://192.168.18.14/api/";
+}
+
+if (process.env.REACT_APP_ENV === "development") {
+  BASE_URL = "http://54.92.203.134/api/";
+}
+
+if (process.env.REACT_APP_ENV === "local") {
+  BASE_URL = "http://192.168.18.14/api/";
+}
+
+export { BASE_URL };
