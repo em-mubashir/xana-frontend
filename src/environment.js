09386ade54bb35479b17c543a9bdb3cd31a9681d
Haseeb Khan <haseeb_f_khan@outlook.com>
2021-12-07 16:38:39 +0500

    environment.js ip changed for server

--- src/environment.js
+++ src/environment.js
@@ -14,10 +14,10 @@
 
 // export { BASE_URL };
 
-let BASE_URL = "http://192.168.18.62:5000/api/";
+let BASE_URL = "http://35.153.58.173:5000/api/";
 
 if (process.env.REACT_APP_ENV === "production") {
-  BASE_URL = "http://192.168.18.62:5000/api/";
+  BASE_URL = "http://35.153.58.173:5000/api/";
 }
 
 if (process.env.REACT_APP_ENV === "development") {
@@ -25,7 +25,7 @@ if (process.env.REACT_APP_ENV === "development") {
 }
 
 if (process.env.REACT_APP_ENV === "local") {
-  BASE_URL = "http://192.168.18.62:5000/api/";
+  BASE_URL = "http://35.153.58.173:5000/api/";
 }
 
 export { BASE_URL };
